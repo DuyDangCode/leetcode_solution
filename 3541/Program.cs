b570a94bba277a7c7a1209987db0e58130439946
Thành Duy <21520774@gm.uit.edu.vn>
2025-09-13 18:29:14 +0700

    3541

---
+++ 3541/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    public static HashSet<char> vowelsSet = new HashSet<char>("ueoai");
+    private static void Main(string[] args)
+    {
+    }
+    public int MaxFreqSum(string s)
+    {
+        var dic = new Dictionary<char, int>();
+        foreach (var c in s)
+        {
+            if (dic.ContainsKey(c))
+            {
+                dic[c] += 1;
+            }
+            else
+            {
+                dic.Add(c, 1);
+            }
+        }
+        int vowelsFreqMax = 0, consonantsFreqMax = 0;
+        foreach (var pair in dic)
+        {
+            if (vowelsSet.Contains(pair.Key))
+            {
+                vowelsFreqMax = Math.Max(vowelsFreqMax, pair.Value);
+            }
+            else
+            {
+                consonantsFreqMax = Math.Max(consonantsFreqMax, pair.Value);
+            }
+        }
+        return vowelsFreqMax + consonantsFreqMax;
+    }
+}
